5c595cd7bd20a9117576e897727216396711d4db
chisom oguine <oguinechisomtimothy@gmail.com>
2023-03-29 14:47:21 +0100

    do comments crud

---
+++ scripts/addComments.js
@@ -0,0 +1,87 @@
+let commentInput = document.getElementById("comment-input");
+let nameInput = document.getElementById("name-comment");
+let avatarInput = document.getElementById("avatar-comment");
+let addBtn = document.getElementById("createBtn");
+const createForm = document.getElementById("createForm");
+const url_string = window.location.href;
+
+// convert to js representation of url  and extracting the param from the url
+let url = new URL(url_string);
+const base_url = "https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1";
+
+const get_id = () => {
+  return url.searchParams.get("id");
+};
+
+// displaying comments
+const displayComments = (data) => {
+  const ulTag = document.querySelector("#comments");
+  const comments = data.map((item) => `<li>${item.comment}</li>`);
+  ulTag.innerHTML = comments;
+};
+
+// getting comments
+const getComments = async (id) => {
+  const commentApi = `${base_url}/news/${id}/comments`;
+  try {
+    let response = await fetch(commentApi);
+    response = await response.json();
+    displayComments(response);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // add comments
+  add.innerText = "add";
+  add.id = "imgAdd";
+  const inputComment = document.getElementById("add");
+
+  // edit comments
+  edit.innerText = "edit";
+  edit.id = "imgEdit";
+
+  // delete comments
+  del.innerText = "delete";
+  del.style.backgroundColor = "red";
+  del.style.color = "#fff";
+  del.style.cursor = "pointer";
+  del.style.border = "none";
+
+  del.onclick = async () => {
+    try {
+      const base_url = "https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1";
+      await fetch(`${base_url}/news/${item.id}/comments/${item.id}`, {
+        method: "DELETE",
+      });
+      location.reload();
+    } catch (e) {
+      alert("An error occurred");
+    }
+  };
+};
+
+const addBtnFn = async () => {
+  try {
+    await fetch(`${base_url}/news/${get_id()}/comments`, {
+      method: "POST",
+      body: JSON.stringify({
+        newsId: get_id(),
+        name: nameInput.value.trim(),
+        avatar: avatarInput.value.trim(),
+        comment: commentInput.value.trim(),
+      }),
+    });
+    // loadingStatus.innerHTML = "<h5>Loading...</h5>";
+    // addBtn.href = `./addComments.html?id=${get_id()}`;
+    window.history.back();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addBtnFn();
+});
+
+// window.onload = getSingleNews(get_id());
--- scripts/newsDetails.js
+++ scripts/newsDetails.js
@@ -1,4 +1,10 @@
+let divider = document.getElementById("divider");
+let add = document.getElementById("add");
+let edit = document.getElementById("edit");
+let del = document.getElementById("del");
 let url_string = window.location.href;
+let loadingDiv = document.getElementById("loadingStatus");
+let jumbotron = document.getElementById("jumbotron");
 
 // convert to js representation of url  and extracting the param from the url
 let url = new URL(url_string);
@@ -10,13 +16,22 @@ const get_id = () => {
 
 const getSingleNews = async (id) => {
   try {
+    jumbotron.style.display = "none";
+    divider.style.display = "none";
+    loadingDiv.innerText = "Loading...";
+
     const response = await fetch(`${base_url}/news/${id}`);
     const newsData = await response.json();
-    console.log("000 >>>>>>>>>> ", newsData);
     await getComments(id);
     displayNewsDetails(newsData);
   } catch (error) {
     console.log(error);
+  } finally {
+    divider.style.display = "block";
+    loadingDiv.innerText = "";
+    jumbotron.style.display = "block";
+    divider.style.display = "flex";
+    divider.style.justifyContent = "space-between";
   }
 };
 
@@ -49,6 +64,43 @@ const getComments = async (id) => {
   } catch (error) {
     console.log(error);
   }
+
+  // add comments
+  add.innerText = "add";
+  add.id = "imgAdd";
+  const inputComment = document.getElementById("add");
+
+  add.onclick = () => {
+    try {
+      loadingStatus.innerHTML = "<h5>Loading...</h5>";
+      add.href = `./addComments.html?id=${get_id()}`;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // edit comments
+  edit.innerText = "edit";
+  edit.id = "imgEdit";
+
+  // delete comments
+  del.innerText = "delete";
+  del.style.backgroundColor = "red";
+  del.style.color = "#fff";
+  del.style.cursor = "pointer";
+  del.style.border = "none";
+
+  del.onclick = async () => {
+    try {
+      const base_url = "https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1";
+      await fetch(`${base_url}/news/${item.id}/comments/${item.id}`, {
+        method: "DELETE",
+      });
+      location.reload();
+    } catch (e) {
+      alert("An error occurred");
+    }
+  };
 };
 
 window.onload = getSingleNews(get_id());
